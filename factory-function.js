f6318078c07d2ed357f507e9c940e688a034d23b
vitorbrangioni <vitorh.brangioni@gmail.com>
2017-09-24 20:49:56 -0300

    Factory Function example

---
+++ factory-function.js
@@ -0,0 +1,13 @@
+var createCounter = function () {
+    var value = 0;
+
+    return {
+        add: function () {
+            return ++value;
+        }
+    };
+};
+
+var counter = createCounter();
+console.log(counter.add());
+console.log(counter.add());
